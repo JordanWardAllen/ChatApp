1c49501dcdaf29c31c5fcfa2fd48dd3b7066ad47
Jordan Ward-Allen <jordan.mech@hotmail.com>
2020-10-05 14:46:19 +1000

    project initialised

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {path: 'chat', component: ChatComponent},
+  {path: '', component: LoginComponent},
+  {path: 'register', component: NewUserComponent},
+  {path: 'profile', component: ProfileComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import { NewUserComponent } from './new-user/new-user.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    LoginComponent,
+    NewUserComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
